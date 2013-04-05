0d652569f64faa79570eac3a2ce2d90759c9cf78
jesus <jemaro_2004@hotmai.com>
2013-04-05 20:43:31 -0300

    Aca ya anda GQGrit, ya recibe los parametros JSON

--- my/listarUsuarios.py
+++ my/listarUsuarios.py
@@ -2,14 +2,42 @@ from utils import login_required
 import flask
 from flask import jsonify
 
-
+class Respuesta():
+    totalPages='1'
+    currPage='1'
+    totalRecords='1'
+    
+    def __init__(self, totalPages,currPage,totalRecords):
+        self.totalPages=totalPages
+        self.currPage=currPage
+        self.totalRecords=totalRecords
+    
+    def jasonizar(self):
+        p="{\"totalpages\" : " + self.totalPages + "\"currpage\" : " + self.currPage + " \"totalrecords\" : " 
+        p= p + self.totalRecords + " ,\"invdata\" : " 
+        
+        p= p+ jsonify(idUsuario="1000", NombreUsuario="resupost",
+                      Nombre="lalal",
+                      Apellido="prerez", 
+                      email="lalalalal")
+        return p
+        
 class ListarUsuarios(flask.views.MethodView):
     @login_required
     def get(self):
-        return flask.render_template('remote.html')
+        r=Respuesta('1','1','1');
+       # return r.jasonizar()
+        return "{\"totalpages\": \"1\", \"currpage\": \"1\",\"totalrecords\": \"1\",\"invdata\" : [{\"NombreUsuario\": \"lafddadsdsalaal\",\"Nombre\": \"lalal\", \"idUsuario\": \"1000\",\"email\": \"lalalalal\", \"Apellido\": \"prerez\"}]}"
         
     @login_required
     def post(self):
-        return jsonify(username="jesus",
-                   email="lalaal",
-                   id="42")
+        return "{\"totalpages\": \"1\", \"currpage\": \"1\",\"totalrecords\": \"1\",\"invdata\" : [{\"NombreUsuario\": \"lafddadsdsalaal\",\"Nombre\": \"lalal\", \"idUsuario\": \"1000\",\"email\": \"lalalalal\", \"Apellido\": \"prerez\"}]}"
+'''
+    
+    
+    jsonify(idUsuario="1000",
+                   NombreUsuario="resupost",
+                   Nombre="lalal",
+                   Apellido="prerez", 
+                   email="lalalalal")
+'''
